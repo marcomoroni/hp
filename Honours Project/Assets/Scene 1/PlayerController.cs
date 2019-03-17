73ef64b5d0214fa7ff60ac867ce90f1063d843c2
Marco Moroni <marcomoroni394@gmail.com>
2019-03-17 23:16:50 +0000

    Small changes

--- Honours Project/Assets/Scene 1/PlayerController.cs
+++ Honours Project/Assets/Scene 1/PlayerController.cs
@@ -6,8 +6,16 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed = 5.0f;
 	public float maxXPos = 3;
+	private Vector3 origin;
+	public float maxZoomIn = 1;
+	public float maxZoomOut = 3;
 
-    void Update()
+	private void Start()
+	{
+		origin = transform.position;
+	}
+
+	void Update()
     {
         if(Input.GetKey("left") && transform.position.x > -maxXPos)
 		{
@@ -17,6 +25,16 @@ public class PlayerController : MonoBehaviour
 		if (Input.GetKey("right") && transform.position.x < maxXPos)
 		{
 			transform.position = transform.position.With(x: transform.position.x + speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey("up") && transform.position.z < origin.z + maxZoomIn)
+		{
+			transform.position = transform.position.With(z: transform.position.z + speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey("down") && transform.position.z > origin.z - maxZoomOut)
+		{
+			transform.position = transform.position.With(z: transform.position.z - speed * Time.deltaTime);
 		}
 	}
 }
