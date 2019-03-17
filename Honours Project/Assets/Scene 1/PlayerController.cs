bbebcdf5918b534523cc661d2b92014660d36932
Marco Moroni <marcomoroni394@gmail.com>
2019-03-17 15:08:23 +0000

    Controllable camera

---
+++ Honours Project/Assets/Scene 1/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 5.0f;
+	public float maxXPos = 3;
+
+    void Update()
+    {
+        if(Input.GetKey("left") && transform.position.x > -maxXPos)
+		{
+			transform.position = transform.position.With(x: transform.position.x - speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey("right") && transform.position.x < maxXPos)
+		{
+			transform.position = transform.position.With(x: transform.position.x + speed * Time.deltaTime);
+		}
+	}
+}
